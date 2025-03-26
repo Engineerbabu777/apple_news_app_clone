fafdf0d27e94a18e4caf6967d4f789c85b4e2d55
Engineerbabu777 <awaismumtaz0099@gmail.com>
2025-03-26 06:36:00 +0500

    `Tabs Files added && group folder added`

---
+++ src/app/(tabs)/audio.tsx
@@ -0,0 +1,19 @@
+
+
+
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+type Props = {}
+
+const AudioScreen = (props: Props) => {
+  return (
+    <View>
+      <Text>AudioScreen</Text>
+    </View>
+  )
+}
+
+export default AudioScreen
+
+const styles = StyleSheet.create({})
---
+++ src/app/(tabs)/following.tsx
@@ -0,0 +1,19 @@
+
+
+
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+type Props = {}
+
+const FollowingScreen = (props: Props) => {
+  return (
+    <View>
+      <Text>FollowingScreen</Text>
+    </View>
+  )
+}
+
+export default FollowingScreen
+
+const styles = StyleSheet.create({})
