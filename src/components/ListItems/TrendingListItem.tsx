e3526cedcb7ddb4e0b9a1b9fca3733c69360acfd
Engineerbabu777 <awaismumtaz0099@gmail.com>
2025-03-26 08:51:57 +0500

    `Trending List Item Done`

--- src/components/ListItems/TrendingListItem.tsx
+++ src/components/ListItems/TrendingListItem.tsx
@@ -1,6 +1,7 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
 import { News } from "@/types/types";
+import NewsListItemFooter from "../NewsListItemFooter";
 
 type Props = {
   newsArticle: News;
@@ -8,8 +9,46 @@ type Props = {
 
 const TrendingListItem = ({ newsArticle }: Props) => {
   return (
-    <View>
-      <Text>Trending</Text>
+    <View style={{ flexDirection: "row", marginBottom: 20, gap: 15 }}>
+      <View
+        style={{
+          backgroundColor: "#FA8128",
+          height: 35,
+          width: 35,
+          borderRadius: 20,
+          alignItems: "center",
+          justifyContent: "center"
+        }}
+      >
+        <Text style={{ color: "white", fontWeight: "bold", fontSize: 18 }}>
+          1
+        </Text>
+      </View>
+      <View
+        style={{
+          gap: 25,
+          flex: 1,
+          borderBottomWidth: 0.5,
+          borderBlockColor: "lightgrey",
+          paddingBottom: 10
+        }}
+      >
+        <View style={{ gap: 5 }}>
+          <Image
+            source={require("@assets/black-logo.png")}
+            style={{ width: 50, height: 20 }}
+            resizeMode="contain"
+          />
+          <Text style={{ fontWeight: "bold", fontSize: 18 }}>
+            {newsArticle.title}
+          </Text>
+        </View>
+
+        <NewsListItemFooter
+          publishedDate={newsArticle.created_at}
+          author={newsArticle.author}
+        />
+      </View>
     </View>
   );
 };
