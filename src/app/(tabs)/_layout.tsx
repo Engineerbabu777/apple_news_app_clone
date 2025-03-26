1f0bebd161a28df6931b0e74e18111ecd3268176
Engineerbabu777 <awaismumtaz0099@gmail.com>
2025-03-26 06:47:14 +0500

    `Tab bar completed`

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,12 +1,64 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
 import { Tabs } from "expo-router";
+import { MaterialCommunityIcons, Entypo } from "@expo/vector-icons";
 
 export default function TabsLayout() {
   return (
-   <Tabs>
-   </Tabs>
+    <Tabs
+      screenOptions={{ headerShown: false, tabBarActiveTintColor: "black" }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Today",
+          tabBarIcon: ({ color }) => (
+            <Entypo name="news" size={24} color={color} />
+          )
+        }}
+      />
+      <Tabs.Screen
+        name="newPlus"
+        options={{
+          title: "News+",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="cards" size={24} color={color} />
+          )
+        }}
+      />
+      <Tabs.Screen
+        name="sports"
+        options={{
+          title: "Sports",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="soccer-field"
+              size={24}
+              color={color}
+            />
+          )
+        }}
+      />
+      <Tabs.Screen
+        name="audio"
+        options={{
+          title: "Audio",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="headphones" size={24} color={color} />
+          )
+        }}
+      />
+      <Tabs.Screen
+        name="following"
+        options={{
+          title: "Following",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="card-search"
+              size={24}
+              color={color}
+            />
+          )
+        }}
+      />
+    </Tabs>
   );
 }
-
-const styles = StyleSheet.create({});
