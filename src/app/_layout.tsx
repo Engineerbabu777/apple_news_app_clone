3752fb019665123ccaa8056f0b61258bbc99699f
Engineerbabu777 <awaismumtaz0099@gmail.com>
2025-03-26 16:33:36 +0500

    `Completed Article Detailed Screen`

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,65 @@
-import { Slot, Stack } from "expo-router";
+import { router, Slot, Stack } from "expo-router";
 import { PaperProvider } from "react-native-paper";
+import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
+import { View } from "react-native";
 
 export default function RootLayout() {
   return (
     <PaperProvider>
-      <Slot />
+      <Stack>
+        <Stack.Screen
+          name="(tabs)"
+          options={{
+            headerShown: false
+          }}
+        />
+        <Stack.Screen
+          name="newsArticle/[id]"
+          options={{
+            headerTitle: "",
+            headerLeft: () => (
+              <Ionicons
+                name="chevron-back"
+                size={25}
+                color={"black"}
+                onPress={() => router.back()}
+              />
+            ),
+            headerRight: () => {
+              return (
+                <View
+                  style={{
+                    gap: 10,
+                    flexDirection: "row",
+                    alignItems: "center"
+                  }}
+                >
+                  <Ionicons
+                    name="share-outline"
+                    size={20}
+                    color={"black"}
+                    style={{
+                      backgroundColor: "#EEEEEE",
+                      borderRadius: 15,
+                      padding: 5
+                    }}
+                  />
+                  <MaterialCommunityIcons
+                    name="dots-horizontal"
+                    size={24}
+                    color={"black"}
+                    style={{
+                      backgroundColor: "#EEEEEE",
+                      borderRadius: 15,
+                      padding: 2
+                    }}
+                  />
+                </View>
+              );
+            }
+          }}
+        />
+      </Stack>
     </PaperProvider>
   );
 }
