76e99a2cd062d5ca531716e35409e4c04c1b5b61
Engineerbabu777 <awaismumtaz0099@gmail.com>
2025-03-26 16:12:11 +0500

    `Files added NewsDetailedArticle`

---
+++ src/app/newsArticle/[id].tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+type Props = {};
+
+const NewsDetailedArticle = (props: Props) => {
+  return (
+    <View>
+      <Text>NewsDetailedArticle</Text>
+    </View>
+  );
+};
+
+export default NewsDetailedArticle;
+
+const styles = StyleSheet.create({});
