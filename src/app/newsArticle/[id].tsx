5f6b2785700592aec4be5ad16df0ad2a6887438c
Engineerbabu777 <awaismumtaz0099@gmail.com>
2025-03-26 16:33:36 +0500

    `Completed Article Detailed Screen`

--- src/app/newsArticle/[id].tsx
+++ src/app/newsArticle/[id].tsx
@@ -1,16 +1,78 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { Stack, useLocalSearchParams } from "expo-router";
+import { View, Text, StyleSheet, ScrollView, Image } from "react-native";
+import allNews from "@assets/data/allNews.json";
+import Markdown from "react-native-markdown-display";
+import { format } from "date-fns";
 
-type Props = {};
+export default function DetailedArticle() {
+  const { id } = useLocalSearchParams();
 
-const NewsDetailedArticle = (props: Props) => {
+  const newsArticle = allNews.find((article) => article.id === id);
+
+  if (!newsArticle) {
+    return (
+      <Text
+        style={{
+          marginTop: 50,
+          textAlign: "center"
+        }}
+      >
+        Article Not Found!
+      </Text>
+    );
+  }
+
+  const { publisher, image, author, created_at, body } = newsArticle;
+
   return (
-    <View>
-      <Text>NewsDetailedArticle</Text>
-    </View>
+    <ScrollView>
+      {/* HEADER TITLE */}
+      <Stack.Screen
+        name="newsArticle/[id]"
+        options={{ headerTitle: publisher.name }}
+      />
+      <Image
+        source={{ uri: image }}
+        style={{ width: "100%", aspectRatio: 3 / 2 }}
+      />
+
+      <View style={{ padding: 25 }}>
+        <View style={{ gap: 10, paddingBottom: 25 }}>
+          <Text style={{ fontSize: 30, fontWeight: "bold" }}>
+            {newsArticle.title}
+          </Text>
+          <Text
+            style={{ fontWeight: "300", color: "grey", fontSize: 12 }}
+          >{`by ${author.name} ${format(
+            created_at,
+            "MMM dd, yyyy hh:mm a"
+          )}`}</Text>
+        </View>
+        <Markdown>{body}</Markdown>
+      </View>
+    </ScrollView>
   );
-};
+}
 
-export default NewsDetailedArticle;
-
-const styles = StyleSheet.create({});
+const markdownStyles = StyleSheet.create({
+  heading1: {
+    fontSize: 32,
+    backgroundColor: "#000000",
+    color: "#FFFFFF"
+  },
+  heading2: {
+    fontSize: 24
+  },
+  heading3: {
+    fontSize: 18
+  },
+  heading4: {
+    fontSize: 16
+  },
+  heading5: {
+    fontSize: 13
+  },
+  heading6: {
+    fontSize: 11
+  }
+});
--- src/components/ListItems/NewsListItem.tsx
+++ src/components/ListItems/NewsListItem.tsx
@@ -3,77 +3,80 @@ import React from "react";
 import { formatDistanceToNow } from "date-fns";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { News } from "@/types/types";
+import { Link } from "expo-router";
 type Props = {
   newsArticle: News;
 };
 
 const NewsListItem = ({ newsArticle }: Props) => {
   return (
-    <View
-      style={{
-        backgroundColor: "white",
-        padding: 10,
-        gap: 10,
-        marginBottom: 15,
-        borderRadius: 10
-      }}
-    >
-      <View style={{ flexDirection: "row" }}>
+    <Link href={`newsArticle/${newsArticle.id}`}>
+      <View
+        style={{
+          backgroundColor: "white",
+          padding: 10,
+          gap: 10,
+          marginBottom: 15,
+          borderRadius: 10
+        }}
+      >
+        <View style={{ flexDirection: "row" }}>
+          <View
+            style={{
+              flexShrink: 1,
+              marginRight: 15,
+              gap: 5
+            }}
+          >
+            <Image
+              source={require("@assets/black-logo.png")}
+              style={{
+                width: 50,
+                height: 20
+              }}
+              resizeMode="contain"
+            />
+            <Text
+              style={{
+                fontSize: 17,
+                fontWeight: "bold"
+              }}
+            >
+              {newsArticle.title}
+            </Text>
+          </View>
+
+          <Image
+            source={{ uri: newsArticle.image }}
+            style={{
+              width: 100,
+              aspectRatio: 1,
+              borderRadius: 10,
+              marginLeft: "auto"
+            }}
+          />
+        </View>
+
         <View
           style={{
-            flexShrink: 1,
-            marginRight: 15,
+            flexDirection: "row",
             gap: 5
           }}
         >
-          <Image
-            source={require("@assets/black-logo.png")}
-            style={{
-              width: 50,
-              height: 20
-            }}
-            resizeMode="contain"
-          />
-          <Text
-            style={{
-              fontSize: 17,
-              fontWeight: "bold"
-            }}
-          >
-            {newsArticle.title}
+          <Text>
+            {formatDistanceToNow(newsArticle.created_at, { addSuffix: true })}
           </Text>
+          <Text>&#x2022;</Text>
+          <Text>{newsArticle.author.name}</Text>
+          <MaterialCommunityIcons
+            name="dots-horizontal"
+            size={22}
+            color="grey"
+            style={{ marginLeft: "auto" }}
+          />
         </View>
-
-        <Image
-          source={{ uri: newsArticle.image }}
-          style={{
-            width: 100,
-            aspectRatio: 1,
-            borderRadius: 10,
-            marginLeft: "auto"
-          }}
-        />
       </View>
-
-      <View
-        style={{
-          flexDirection: "row",
-          gap: 5
-        }}
-      >
-        <Text>
-          {formatDistanceToNow(newsArticle.created_at, { addSuffix: true })}
-        </Text>
-        <Text>&#x2022;</Text>
-        <Text>{newsArticle.author.name}</Text>
-        <MaterialCommunityIcons
-          name="dots-horizontal"
-          size={22}
-          color="grey"
-          style={{ marginLeft: "auto" }}
-        />
-      </View>
-    </View>
+    </Link>
   );
 };
 
